76e5827a52a21205b281cb71645aa269ff3c9bc9
wolfy <jinming.zhang47@gmail.com>
2023-06-23 01:05:57 -0700

    initial commit

---
+++ Source/Blaaaster/BlaaasterGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "BlaaasterGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class BLAAASTER_API ABlaaasterGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
